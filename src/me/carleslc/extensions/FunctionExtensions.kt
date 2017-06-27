bea80835553b3be94d2eace612f929dcc34114b8
Carleslc <lazaro.costa.carles@gmail.com>
2017-06-28 00:01:07 +0200

    Added composition and time extensions

---
+++ src/me/carleslc/extensions/FunctionExtensions.kt
@@ -0,0 +1,9 @@
+package me.carleslc.extensions
+
+public fun <A, B> compose(f: (A) -> A,
+		g: (A) -> B): (A) -> B = { x -> g(f(x)) }
+
+
+public fun <A, B> compose(f: (A) -> A,
+		g: (A) -> A,
+		h: (A) -> B): (A) -> B = { x -> h(g(f(x))) }
---
+++ src/me/carleslc/extensions/TimeExtensions.kt
@@ -0,0 +1,29 @@
+package me.carleslc.extensions
+
+private val SECOND = 1000f
+private val MINUTE = 60 * SECOND
+private val HOUR = 60 * MINUTE
+private val DAY = 24 * HOUR
+
+public fun getTimeFrom(startMillis: Long): String {
+	var millis = System.currentTimeMillis() - startMillis
+	val builder = StringBuilder()
+	if (millis > DAY) {
+		builder.append(Math.round(millis / DAY)).append(" d ")
+		millis %= DAY.toLong()
+	}
+	if (millis > HOUR) {
+		builder.append(Math.round(millis / HOUR)).append(" h ")
+		millis %= HOUR.toLong()
+	}
+	if (millis > MINUTE) {
+		builder.append(Math.round(millis / MINUTE)).append(" m ")
+		millis %= MINUTE.toLong()
+	}
+	if (millis > SECOND) {
+		builder.append(Math.round(millis / SECOND)).append(" s ")
+		millis %= SECOND.toLong()
+	}
+	if (millis > 0 || builder.length == 0) builder.append(millis).append(" ms")
+	return builder.toString().trim()
+}
