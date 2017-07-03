43c5eed5e1ec7e26538aae9832232ee24067bad7
Carleslc <lazaro.costa.carles@gmail.com>
2017-07-03 14:56:20 +0200

    Times indexed from 0 instead 1

--- src/me/carleslc/kotlin/extensions/utils/UtilsExtensions.kt
+++ src/me/carleslc/kotlin/extensions/utils/UtilsExtensions.kt
@@ -1,5 +1,5 @@
 package me.carleslc.kotlin.extensions.utils
 
-public inline fun <T> Int.timesIndexed(predicate: (Int) -> T) = (1..this).forEach { predicate(it) }
+public inline fun <T> Int.timesIndexed(predicate: (Int) -> T) = (0..this - 1).forEach { predicate(it) }
 
 public inline fun <T> Int.times(predicate: () -> T) = timesIndexed { predicate() }
