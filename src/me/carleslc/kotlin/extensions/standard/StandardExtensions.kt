11c07f2c515198ae1c05115fedcf1d295397fcec
Carleslc <lazaro.costa.carles@gmail.com>
2017-07-04 06:39:39 +0200

    Optimized imports

--- src/me/carleslc/kotlin/extensions/standard/StandardExtensions.kt
+++ src/me/carleslc/kotlin/extensions/standard/StandardExtensions.kt
@@ -1,8 +1,5 @@
 package me.carleslc.kotlin.extensions.standard
 
-import me.carleslc.kotlin.extensions.number.*
-import org.funktionale.tries.Try
-
 public fun FIXME(): Nothing = throw Error("An operation needs a fix.")
 
 public fun FIXME(reason: String): Nothing = throw Error("An operation needs a fix: $reason")
