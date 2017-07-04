b6f82d269b318703bf105ff76e8c6d9b155130bf
Carleslc <lazaro.costa.carles@gmail.com>
2017-07-04 02:53:02 +0200

    FIXME method

--- src/me/carleslc/kotlin/extensions/standard/StandardExtensions.kt
+++ src/me/carleslc/kotlin/extensions/standard/StandardExtensions.kt
@@ -1,5 +1,9 @@
 package me.carleslc.kotlin.extensions.standard
 
+public fun FIXME(): Nothing = throw Error("An operation needs a fix.")
+
+public fun FIXME(reason: String): Nothing = throw Error("An operation needs a fix: $reason")
+
 public inline infix fun Int.times(predicate: (Int) -> Unit) = repeat(this, predicate)
 
 public inline fun <T, R> T?.letOr(nullValue: R, block: (T) -> R) = this?.let(block) ?: nullValue
