234c1806d0b8cae2fe520e570131748b355e5505
Carleslc <lazaro.costa.carles@gmail.com>
2017-07-09 22:18:41 +0200

    String.to(primitive)orThrow with lazy throwable

--- src/me/carleslc/kotlin/extensions/conversions/ConversionExtensions.kt
+++ src/me/carleslc/kotlin/extensions/conversions/ConversionExtensions.kt
@@ -46,38 +46,45 @@ inline fun Double.toBinaryString(): String = toBits().toBinaryString()
 inline fun Float.toBinaryString(): String = toBits().toBinaryString()
 
 inline fun String.toByteOrNull(): Byte? = Try { java.lang.Byte.parseByte(this) }.getOrNull()
-inline fun String.toByteOrRun(crossinline defaultBlock: (String) -> Byte): Byte = toByteOrNull() ?: run(defaultBlock)
-inline fun String.toByteOrThrow(throwable: Throwable): Byte = toByteOrRun { throw throwable }
+inline fun String.toByteOrRun(defaultBlock: (String) -> Byte): Byte = toByteOrNull() ?: run(defaultBlock)
+inline fun String.toByteOrThrow(crossinline throwable: String.() -> Throwable): Byte = toByteOrRun { throw throwable() }
+inline fun String.toByteOrThrow(throwable: Throwable): Byte = toByteOrThrow { throwable }
 inline fun String.toByteOrDefault(default: Byte = 0): Byte = toByteOrRun { default }
 
 inline fun String.toShortOrNull(): Short? = Try { java.lang.Short.parseShort(this) }.getOrNull()
-inline fun String.toShortOrRun(crossinline defaultBlock: (String) -> Short): Short = toShortOrNull() ?: run(defaultBlock)
-inline fun String.toShortOrThrow(throwable: Throwable): Short = toShortOrRun { throw throwable }
+inline fun String.toShortOrRun(defaultBlock: (String) -> Short): Short = toShortOrNull() ?: run(defaultBlock)
+inline fun String.toShortOrThrow(crossinline throwable: String.() -> Throwable): Short = toShortOrRun { throw throwable() }
+inline fun String.toShortOrThrow(throwable: Throwable): Short = toShortOrThrow { throwable }
 inline fun String.toShortOrDefault(default: Short = 0): Short = toShortOrRun { default }
 
 inline fun String.toIntOrNull(): Int? = Try { java.lang.Integer.parseInt(this) }.getOrNull()
-inline fun String.toIntOrRun(crossinline defaultBlock: (String) -> Int): Int = toIntOrNull() ?: run(defaultBlock)
-inline fun String.toIntOrThrow(throwable: Throwable): Int = toIntOrRun { throw throwable }
+inline fun String.toIntOrRun(defaultBlock: (String) -> Int): Int = toIntOrNull() ?: run(defaultBlock)
+inline fun String.toIntOrThrow(crossinline throwable: String.() -> Throwable): Int = toIntOrRun { throw throwable() }
+inline fun String.toIntOrThrow(throwable: Throwable): Int = toIntOrThrow { throwable }
 inline fun String.toIntOrDefault(default: Int = 0): Int = toIntOrRun { default }
 
 inline fun String.toLongOrNull(): Long? = Try { java.lang.Long.parseLong(this) }.getOrNull()
-inline fun String.toLongOrRun(crossinline defaultBlock: (String) -> Long): Long = toLongOrNull() ?: run(defaultBlock)
-inline fun String.toLongOrThrow(throwable: Throwable): Long = toLongOrRun { throw throwable }
+inline fun String.toLongOrRun(defaultBlock: (String) -> Long): Long = toLongOrNull() ?: run(defaultBlock)
+inline fun String.toLongOrThrow(crossinline throwable: String.() -> Throwable): Long = toLongOrRun { throw throwable() }
+inline fun String.toLongOrThrow(throwable: Throwable): Long = toLongOrThrow { throwable }
 inline fun String.toLongOrDefault(default: Long = 0): Long = toLongOrRun { default }
 
 inline fun String.toFloatOrNull(): Float? = Try { java.lang.Float.parseFloat(this) }.getOrNull()
-inline fun String.toFloatOrRun(crossinline defaultBlock: (String) -> Float): Float = toFloatOrNull() ?: run(defaultBlock)
-inline fun String.toFloatOrThrow(throwable: Throwable): Float = toFloatOrRun { throw throwable }
+inline fun String.toFloatOrRun(defaultBlock: (String) -> Float): Float = toFloatOrNull() ?: run(defaultBlock)
+inline fun String.toFloatOrThrow(crossinline throwable: String.() -> Throwable): Float = toFloatOrRun { throw throwable() }
+inline fun String.toFloatOrThrow(throwable: Throwable): Float = toFloatOrThrow { throwable }
 inline fun String.toFloatOrDefault(default: Float = 0f): Float = toFloatOrRun { default }
 
 inline fun String.toDoubleOrNull(): Double? = Try { java.lang.Double.parseDouble(this) }.getOrNull()
-inline fun String.toDoubleOrRun(crossinline defaultBlock: (String) -> Double): Double = toDoubleOrNull() ?: run(defaultBlock)
-inline fun String.toDoubleOrThrow(throwable: Throwable): Double = toDoubleOrRun { throw throwable }
+inline fun String.toDoubleOrRun(defaultBlock: (String) -> Double): Double = toDoubleOrNull() ?: run(defaultBlock)
+inline fun String.toDoubleOrThrow(crossinline throwable: String.() -> Throwable): Double = toDoubleOrRun { throw throwable() }
+inline fun String.toDoubleOrThrow(throwable: Throwable): Double = toDoubleOrThrow { throwable }
 inline fun String.toDoubleOrDefault(default: Double = 0.0): Double = toDoubleOrRun { default }
 
 inline fun String.toBooleanOrNull(): Boolean? = if (equals("true", ignoreCase = true)) true else if (equals("false", ignoreCase = true)) false else null
 inline fun String.toBooleanOrRun(defaultBlock: (String) -> Boolean): Boolean = toBooleanOrNull() ?: run(defaultBlock)
-inline fun String.toBooleanOrThrow(throwable: Throwable): Boolean = toBooleanOrRun { throw throwable }
+inline fun String.toBooleanOrThrow(crossinline throwable: String.() -> Throwable): Boolean = toBooleanOrRun { throw throwable() }
+inline fun String.toBooleanOrThrow(throwable: Throwable): Boolean = toBooleanOrThrow { throwable }
 inline fun String.toBooleanOrDefault(default: Boolean = false): Boolean = toBooleanOrRun { default }
 
 inline fun Any?.toByte(): Byte {
