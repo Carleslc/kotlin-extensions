65808d0bdd55ed6cf521ab50bb68dd84a2ba90b8
Paramvir Singh <ps17cw@brocku.ca>
2023-05-04 17:55:37 -0400

    trail test working

--- src/test/kotlin/strings/StringsExtensionsKtTest.kt
+++ src/test/kotlin/strings/StringsExtensionsKtTest.kt
@@ -1,11 +1,19 @@
 package strings
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 
 class StringsExtensionsKtTest {
 
-    @org.junit.jupiter.api.Test
-    fun uniquifyWords() {
-        assertEquals(4,2)
+    @Test
+    fun uniquifyWordsTest() {
+
+        val input = "java kotlin java c++ python python rust"
+        val expected = "java kotlin c++ python rust"
+        val actual = input.uniquifyWords()
+
+        Assertions.assertEquals(expected, actual)
+
     }
 }
